d39d0ed286f80eebac4f9fa3a038f8e6729a5808
WOW.MR.NIKI❤️ <126978356+VihangaHansana@users.noreply.github.com>
2024-08-11 20:25:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/a6613fc154a69d1ce94d4.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HEY, THE NIKI BOT IS ONLINE NOW 💃💗. ",
 };
