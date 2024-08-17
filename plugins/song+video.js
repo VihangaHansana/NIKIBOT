c43c7f97c3a57979d30b909d5eb6fa2e66c94d57
WOW.MR.NIKI❤️ <126978356+VihangaHansana@users.noreply.github.com>
2024-08-17 10:44:09 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,1 +1,34 @@
+const {cmd , commands} = require('../command')
 
+const fg = require('api-dylux')
+const yts = require('yt-search')
+cmd({
+    pattern: "song",
+    desc: "download songs",
+    category: "download ",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or title ")
+const search = await yts(q)
+const data =search.video[0];
+const url =data.url
+let desc = '
+⭐ *NIKI BOT SONG DOWNLOADER * ⭐
+  title : ${data.title}
+  description: ${data.description}
+  time:{${data.timestamp}
+  ago:${data.ago}
+  views: ${data.views}
+
+
+MADE BY NIKI BOT 💃💗
+'
+
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc,{quoted:mek});     
+}catch(e){
+console.log(e)
+reply('${e}')
+}
+})
