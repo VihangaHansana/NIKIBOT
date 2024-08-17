38e92c5153142fda22e206bb43c828c68d6d4f4f
WOW.MR.NIKI❤️ <126978356+VihangaHansana@users.noreply.github.com>
2024-08-17 13:03:05 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -33,11 +33,59 @@ let down = await.fg yta(url)
 let downloadUrl = down.dl_url
 
 
-//send audio massage 
+//send audio+document massage 
 
 
 await con.sendMessage (from, {audio: {url:downloadUrl}, mimetype:"audio/mpeg"},{quoted:mek})
        
+}catch(e){
+console.log(e)
+reply('${e}')
+}
+})
+
+//==============download-Video===============
+cmd({
+    pattern: "video",
+    desc: "download videos",
+    category: "download ",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or title ")
+const search = await yta(q)
+const data =search.video[0];
+const url =data.url
+let desc = '
+⭐ *NIKI BOT VIDEOS DOWNLOADER * ⭐
+  title : ${data.title}
+  description: ${data.description}
+  time:{${data.timestamp}
+  ago:${data.ago}
+  views: ${data.views}
+
+
+MADE BY NIKI BOT 💃💗
+'
+
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc,{quoted:mek});    
+await con.sendMessage (from, {document: {url:downloadUrl}, mimetype:"audio/mpeg",fileName:data.title + ".mp3", caption:"*POWERED BY NIKI BOT 💃💗*"},{quoted:mek})
+
+        
+//download video
+let down = await.fg ytv(url)
+let downloadUrl = down.dl_url
+
+
+//send video+document massage 
+
+
+await con.sendMessage (from, {video: {url:downloadUrl}, mimetype:"video/mp4"},{quoted:mek})
+
+
+await con.sendMessage (from, {document: {url:downloadUrl}, mimetype:"video/mp4",fileName:data.title + ".mp4", caption:"*POWERED BY NIKI BOT 💃💗*"},{quoted:mek})
+  
 }catch(e){
 console.log(e)
 reply('${e}')
