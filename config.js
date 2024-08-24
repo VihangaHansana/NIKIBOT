b1eb241a1c46f0a23d70fd345584702d4cb63456
WOW.MR.NIKI❤️ <126978356+VihangaHansana@users.noreply.github.com>
2024-08-24 13:03:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/a6613fc154a69d1ce94d4.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HEY, THE NIKI BOT IS ONLINE NOW 💃💗. ",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "private", 
+MONGODB: process.env.MONGODB || "ENTER MONGODB URL",
 };
